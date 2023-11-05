6a992ced9a672d73b31924087ffd2958755c81e6
skalexet <alex5642har@gmail.com>
2023-11-05 16:51:06 +0400

    feat #1: hypertext min

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
-const cleanCSS = require('gulp-clean-css');
 
 function buildAll(cb) {
   gulp.src('*.html')
     .pipe(concat('index.html'))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist/'));
 
   gulp.src('src/*.css')
@@ -14,6 +17,7 @@ function buildAll(cb) {
     .pipe(gulp.dest('dist/'));
 
   gulp.src('src/*.js')
+    .pipe(babel({presets: ['@babel/env']}))
     .pipe(concat('script.js'))
     .pipe(terser())
     .pipe(gulp.dest('dist/'));
