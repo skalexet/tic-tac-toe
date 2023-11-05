602e74e81606fc927f368389be3c36b2c7012dbb
skalexet <alex5642har@gmail.com>
2023-11-05 17:21:53 +0400

    feat #1: gulp comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+/**
+ * Modules Import
+ */
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
@@ -5,24 +8,34 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
 
+
+/**
+ * Running function
+ */
 function buildAll(cb) {
+  
+  /* Minifies hypertext */
   gulp.src('*.html')
     .pipe(concat('index.html'))
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist/'));
 
+  /** Minifies stylesheets */
   gulp.src('src/*.css')
     .pipe(concat('styles.css'))
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist/'));
 
+  /** Transpiles javascript 2015+ syntax into javascript 2015- syntax, minifies transpiled javascript code */
   gulp.src('src/*.js')
     .pipe(babel({presets: ['@babel/env']}))
     .pipe(concat('script.js'))
     .pipe(terser())
     .pipe(gulp.dest('dist/'));
 
+  /** Native callback */
   cb();
 }
 
+/** Entry point for gulp script */
 gulp.task('default', buildAll);
